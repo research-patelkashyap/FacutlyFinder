a8cefb59261777664e47f5494c52fa4c84e8cd64
meet272002 <81922396+meet272002@users.noreply.github.com>
2026-02-05 10:05:16 +0530

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -1,10 +1,10 @@
 set -e
 
 echo "Running scraper..."
-python -m scrape.dau.main.py
+python -m scrape.dau.main.py &
 
 echo "Initializing database..."
-python main.py &
+python main.py
 
 echo "Starting FastAPI..."
 uvicorn FastAPI:app --host 0.0.0.0 --port $PORT
